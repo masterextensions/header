67e7bb4f72c2b01f289c1576ffe1c2c9695ebfe7
Pouriya <behinenet@outlook.com>
2020-12-31 09:26:50 +0330

    fix some problems

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,31 +1,31 @@
-import * as path from 'path';
+// import * as path from 'path';
 
-import  { runTests } from 'vscode-test';
+// import  { runTests } from 'vscode-test';
 
-async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+// async function main() {
+// 	try {
+// 		// The folder containing the Extension Manifest package.json
+// 		// Passed to `--extensionDevelopmentPath`
+// 		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
-
-
-		const testOptions = {
-			extensionPath: extensionDevelopmentPath,
-			testRunnerPath: extensionTestsPath
-		}
+// 		// The path to test runner
+// 		// Passed to --extensionTestsPath
+// 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests(testOptions);
+// 		const testOptions = {
+// 			extensionPath: extensionDevelopmentPath,
+// 			testRunnerPath: extensionTestsPath
+// 		}
+
+
+// 		// Download VS Code, unzip it and run the integration test
+// 		await runTests(testOptions);
 		
-	} catch (err) {
-		console.error('Failed to run tests');
-		process.exit(1);
-	}
-}
+// 	} catch (err) {
+// 		console.error('Failed to run tests');
+// 		process.exit(1);
+// 	}
+// }
 
-main();
+// main();
